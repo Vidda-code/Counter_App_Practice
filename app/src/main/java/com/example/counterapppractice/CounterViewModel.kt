3dfb38cafb42c3b31a4eecefc494bbe4a40be06d
David Godfrey <kinqvidda@gmail.com>
2026-05-25 21:10:05 +0100

    implemented the CounterViewModel state

--- app/src/main/java/com/example/counterapppractice/CounterViewModel.kt
+++ app/src/main/java/com/example/counterapppractice/CounterViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.counterapppractice
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 data class Counterstate(
     val count: Int = 0,
     val history: List<String> = emptyList()
@@ -10,4 +14,9 @@ data class Counterstate(
             count < 0 -> "Negative"
             else -> "Zero"
         }
+}
+
+class CounterViewModel : ViewModel() {
+    private val _state = MutableStateFlow(Counterstate())
+    val state = _state.asStateFlow()
 }

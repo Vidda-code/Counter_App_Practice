faeb6d06e9eca065a28b17acf0139c52a2b7088f
David Godfrey <kinqvidda@gmail.com>
2026-05-26 11:22:05 +0100

    implementing the HistoryScreen

---
+++ app/src/main/java/com/example/counterapppractice/ui/HistoryScreen.kt
@@ -0,0 +1,25 @@
+package com.example.counterapppractice.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HistoryScreen(
+    modifier: Modifier = Modifier,
+    history: List<String>,
+    listState: LazyListState = rememberLazyListState()
+) {
+    LazyColumn(
+        state = listState,
+        modifier = Modifier
+    ) {
+        items(history) { item ->
+            Text(text = "History: $history")
+        }
+    }
+}
